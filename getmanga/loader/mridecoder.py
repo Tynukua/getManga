e77f42a2d9e049892da23fa12b1912f83383592f
Tynukua <bowsworddv@gmail.com>
2019-11-16 14:23:52 +0200

    add conver to mridecoder

--- getmanga/loader/mridecoder.py
+++ getmanga/loader/mridecoder.py
@@ -1,6 +1,18 @@
-    
-def mri_decoder(data):
+from PIL import Image
+from io import BytesIO
+RGB = 'RGB'
+JPEG ='JPEG'
+
+def mri_decoder(file):
+    data = file.getvalue()
     n = len(data) + 7
-    header = [82, 73, 70, 70, 255 & n, n >> 8 & 255, n >> 16 & 255, n >> 24 & 255, 87, 69, 66, 80, 86, 80, 56]
+    header = [82, 73, 70, 70, 255 & n, 
+        n >> 8 & 255, n >> 16 & 255, 
+        n >> 24 & 255, 87, 69, 66, 80, 
+        86, 80, 56]
     data = list(map(lambda x: x ^ 101, data))
-    return bytes(header + data)
+    file = BytesIO()
+    file.write(bytes(header + data))
+    image = Image.open(file)
+    image.convert(RGB).save(file, JPEG )
+    return file
