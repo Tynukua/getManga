8c3606f2243b395a73b1c1cbadaffcc52dbe4886
Tynukua <bowsworddv@gmail.com>
2019-11-16 13:07:42 +0200

    0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name = 'getmanga',
-    version="0.1.3",
+    version="0.1.4",
     author="Tynukua",
     author_email = 'tynuk.ua@gmail.com',
     description = 'package for load manga! :)',
