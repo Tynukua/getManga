3613014dd072a9de24108aba29c72c156e707195
Tynukua <bowsworddv@gmail.com>
2019-11-22 16:02:36 +0200

    mridecoder

--- getmanga/format/converter.py
+++ getmanga/format/converter.py
@@ -1,2 +1,12 @@
 class Converter:
-    pass
+    def __init__(self, path_list):
+        self.path_list = path_list
+
+    def mri_decoder(self, data):
+        n = len(data) + 7
+        header = [82, 73, 70, 70, 255 & n, 
+            n >> 8 & 255, n >> 16 & 255, 
+            n >> 24 & 255, 87, 69, 66, 80, 
+            86, 80, 56]
+        data = list(map(lambda x: x ^ 101, data))
+        return bytes(header + data)
